fad75d9cc15cdfe90b0881408f548b28cb51b0f1
Michal <michalprelich@gmail.com>
2019-12-05 18:32:48 +0100

    day 3 part 1 works

--- day3/day3p1.py
+++ day3/day3p1.py
@@ -5,17 +5,20 @@ p2 = f2.read()
 path1 = p1.split(",")
 path2 = p2.split(",")
 
+
 class  Point:
-	def __init__(self, x, y)
+	def __init__(self, x, y):
 		self.px = x
 		self.py = y
+
+	def sumofcoords(self):
+		return abs(self.px) + abs(self.py)
 
 
 def move(line):
 	x, y = 0, 0
+	pointslist = []
 	for m in line:
-		pointslist = []
-		pointslist.append(Point(x, y))
 		direction = m[0]
 		steps = int(m[1:])
 		for _ in range(steps):
@@ -27,8 +30,23 @@ def move(line):
 				y = y + 1
 			elif direction == 'D':
 				y = y - 1
-			pointslist.append(Point(x, y))
+			p = Point(x, y)
+			pointslist.append(p)
 	return pointslist
 
+
 list1 = move(path1)
-list2 = mode(path2)
+list2 = move(path2)
+
+set1 = set((p.px, p.py) for p in list1)
+inter = [p for p in list2 if (p.px, p.py) in set1]
+
+maxint = 1000000
+for i in inter:
+	addcoords = i.sumofcoords()
+	if addcoords < maxint:
+		maxint = addcoords
+
+print(maxint)
+
+
