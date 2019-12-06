45bd04f4baf55cc42d43aaa830102f49bde85867
Michal <michalprelich@gmail.com>
2019-12-06 18:16:03 +0100

    very important message

--- day5/day5p1.py
+++ day5/day5p1.py
@@ -13,7 +13,8 @@ def code(noun: int, verb: int):
     ints_copy = ints.copy()
     ints_copy[1] = noun
     ints_copy[2] = verb
-    for i in range(0, n-1, 4):
+    instruction = ints_copy[i]
+    while instruction != 99:
         instruction = ints_copy[i]
         i2 = ints_copy[i+1]
         i3 = ints_copy[i+2]
--- day6/day6p1.py
+++ day6/day6p1.py
@@ -1,17 +1,1 @@
-nf = open("file.txt", "r")
-
-file = nf.readlines()
-
-f = {}				# dict with string and list of strings
-o = { 'COM': 0 }	# dict with string and int
-
-for line in file:
-	x, y = line.split(')')
-	if x not in f:
-		f[x] = []	# new list for every object
-	f[x].append(y)
-
-
-
-
-nf.close()
+# todo
