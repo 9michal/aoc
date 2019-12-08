28eb2b6a3e7ce7f8f05b14fbac4e4bc3655bb4b8
Michal <michalprelich@gmail.com>
2019-12-08 23:51:10 +0100

    day8p2

--- day6/day6p1.py
+++ day6/day6p1.py
@@ -1,1 +1,22 @@
-# todo
+# orbits = {}
+# f = open('file.txt')
+# f = list(f.readlines())
+
+# for o in f:
+# 	n1, n2 = o.rstrip().split(')')
+# 	orbits[n2] = n1
+
+
+# def count(orbits, node):
+# 	allOrb = 0
+# 	curr_node = node
+# 	while orbits.get(curr_node) != None:
+# 		curr_node = orbits.get(curr_node)
+# 		allOrb += 1
+# 	return allOrb
+
+# allOrbits = 0
+# for node in orbits.keys():
+# 	allOrbits += count(orbits, node)
+
+# print(allOrbits)
---
+++ day8/day8p2.py
@@ -0,0 +1,15 @@
+file = open("file.txt", "r")
+
+ints = file.read()
+file.close()
+
+x, y = 25, 6
+size = x * y
+
+for i in range(size):
+	for j in range(size):
+		if ints[(j*size)+i] != "2":	# 2 is transparent, doesnt matter
+			print("@" if ints[(j*size)+i] == "1" else " ", end="")
+			break
+	if (i+1) % x == 0:
+		print()
