bd86b3c60de876ed9352780f0bce4a14838ea284
Michal <michalprelich@gmail.com>
2019-12-10 17:22:53 +0100

    both parts work

--- day4/day4p1.py
+++ day4/day4p1.py
@@ -10,8 +10,6 @@ def twoDigits(x):
 def notDecrease(x):
 	v = True
 	x = str(x)
-#	if list(x) == sorted(x):
-#		v = False
 	for i in range(5):
 		if x[i+1] < x[i]:
 			v = False
@@ -19,6 +17,7 @@ def notDecrease(x):
 	return v
 
 numOfPasswords = 0
+
 for i in range(193651, 649730): # Your puzzle input is 193651-649729.
 	if twoDigits(i) & notDecrease(i):
 		numOfPasswords = numOfPasswords + 1
--- day4/day4p2.py
+++ day4/day4p2.py
@@ -18,8 +18,6 @@ def twoDigits(x):
 def notDecrease(x):
 	v = True
 	x = str(x)
-#	if list(x) == sorted(x):
-#		v = False
 	for i in range(5):
 		if x[i+1] < x[i]:
 			v = False
@@ -27,6 +25,7 @@ def notDecrease(x):
 	return v
 
 numOfPasswords = 0
+
 for i in range(193651, 649730): # Your puzzle input is 193651-649729.
 	if twoDigits(i) & notDecrease(i):
 		numOfPasswords = numOfPasswords + 1
