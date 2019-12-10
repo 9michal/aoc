50ba3aa858916ee6b0a5d2c4d26efb5b0d2bc1e0
Michal <michalprelich@gmail.com>
2019-12-10 16:50:43 +0100

    day 5

--- day5/day5p1.py
+++ day5/day5p1.py
@@ -10,7 +10,7 @@ for i in range(len(ints)):
 i = 0
 while str(ints[i])[:-3:-1] != "99": # DE (last two numbers) in opcode is not 99
     full = str(ints[i]).zfill(5) # zfill is for 0 at the begining of string
-    instruction = int(full[3:]) # DE
+    instruction = int(full[3:]) # E
 
     if (instruction == 1):
         i1 = ints[i + 1] if full[2] == '1' else ints[ints[i + 1]]
