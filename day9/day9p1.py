5a3c66d7b4f4fbb7ecfd4a33170bcfc4db536504
Michal <michalprelich@gmail.com>
2019-12-10 16:51:25 +0100

    day 9 after 203

--- day9/day9p1.py
+++ day9/day9p1.py
@@ -1,64 +1,78 @@
-file = open("file.txt", "r")
+file = open("/home/michal/Desktop/AOC/day9/file.txt", "r")
 
-ints = file.read()
+ints = file.readline()
 ints = ints.split(",")
 file.close()
 
-for i in range(len(ints)):
-    ints[i] = int(ints[i])
-
+ints = [int(x) for x in ints] + [0] * 1000
 relative = 0
 i = 0
 
-
 while str(ints[i])[:-3:-1] != '99':     # DE (last two numbers) in opcode is not 99
     full = str(ints[i]).zfill(5)        # zfill is for 0 at the begining of string
-    instruction = int(full[3:])         # DE
+    instruction = int(full[3:])         # E
 
-    if instruction in [1, 2, 4, 5, 6, 7, 8, 9]:
-        i1 = ints[i + 1] if full[2] == '1' else ints[ints[i + 1] + relative] if full[2] == '2' else ints[ints[i + 1]]
-    if instruction in [1, 2, 4, 5, 6, 7, 8]:
-        i2 = ints[i + 2] if full[1] == '1' else ints[ints[i + 2] + relative] if full[1] == '2' else ints[ints[i + 2]]
-    if instruction in [1, 2, 7, 8]:
-        i3 = ints[i + 3]
+    if (instruction in [1, 2, 4, 5, 6, 7, 8, 9]):
+        if full[2] == '1':
+            i1 = i + 1
+        elif full[2] == '2':
+            i1 = ints[i + 1] + relative
+        elif full[2] == '0':
+            i1 = ints[i + 1]
+
+    if (instruction in [1, 2, 5, 6, 7, 8]):
+        if full[1] == '1':
+            i2 = i + 2
+        elif full[1] == '2':
+            i2 = ints[i + 2] + relative
+        elif full[1] == '0':
+            i2 = ints[i + 2]
+
+    if (instruction in [1, 2, 7, 8]):
+        if full[0] == '2':
+            i3 = ints[i + 3] + relative
+        elif full[0] == '0':
+            i3 = ints[i + 3]
 
     if (instruction == 1):
-        ints[i3] = i1 + i2
-        i += 4
-        
+        ints[i3] = ints[i1] + ints[i2]
+        i += 4   
+
     elif (instruction == 2):
-        ints[i3] = i1 * i2
+        ints[i3] = ints[i1] * ints[i2]
         i += 4
 
     elif (instruction == 3):
-        ints[ints[i + 1]] = 5 #input 5
+        if full[2] == '0' or full[2] == '1':
+            ints[ints[i + 1]] = 1
+        elif full[2] == '2':
+            ints[ints[i + 1] + relative] = 1
         i += 2
 
     elif (instruction == 4):
-        i1 = ints[i + 1] if full[2] == '1' else ints[ints[i + 1]]
-        print(i1)
+        print(ints[i1])
         i += 2
 
     elif (instruction == 5):
-        if (instruction == 5 and i1):
-            i = i2
+        if (ints[i1] != 0):
+            i = ints[i2]
         else:
             i += 3
 
     elif (instruction == 6):
-        if (instruction == 6 and not i1):
-            i = i2
+        if (ints[i1] == 0):
+            i = ints[i2]
         else:
             i += 3
 
     elif (instruction == 7):
-        ints[i3] = i1 < i2
+        ints[i3] = 1 if ints[i1] < ints[i2] else 0
         i += 4
 
     elif (instruction == 8):
-        ints[i3] = i1 == i2
+        ints[i3] = 1 if ints[i1] == ints[i2] else 0
         i += 4
 
     elif (instruction == 9):
-        relative += i1
+        relative += ints[i1]
         i += 2
