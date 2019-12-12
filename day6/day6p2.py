37dad0e88be7c47d4df0418e7cccdb74d67d2401
Michal <michalprelich@gmail.com>
2019-12-12 12:43:34 +0100

    day6 works

--- day6/day6p2.py
+++ day6/day6p2.py
@@ -7,13 +7,13 @@ orbits = {}
 for line in file:
     pt = line.split(')')
     pt1 = pt[0]
-    pt2 = pt[1][:3] # '\n' at end: XXX\n
+    pt2 = pt[1][:3] # '\n' at end: "XXX\n"
     allPlanets.add(pt1)
     allPlanets.add(pt2)
     orbits[pt2] = pt1  
 
 
-def getPathToStart(startPlanet, ):
+def getPathToStart(startPlanet):
     listOfPlanets = []
     planet = startPlanet
     while planet in orbits.keys():
@@ -26,7 +26,7 @@ pathYOU = getPathToStart('YOU')
 intersect = [x for x in pathSAN if x in pathYOU]
 intersectFirst = intersect[0]
 
-def pathL(startPlanet, inter):
+def pathLen(startPlanet, inter):
     planet = startPlanet
     pathLength = 0
     while planet in orbits.keys() and planet != inter:
@@ -34,5 +34,5 @@ def pathL(startPlanet, inter):
         pathLength += 1
     return pathLength
 
-pathSANtoYOU = pathL('SAN', intersectFirst) + pathL('YOU', intersectFirst) - 2 # -2 because "Between the objects they are orbiting - not between YOU and SAN."
+pathSANtoYOU = pathLen('SAN', intersectFirst) + pathLen('YOU', intersectFirst) - 2 # -2 because "Between the objects they are orbiting - not between YOU and SAN."
 print(pathSANtoYOU)
