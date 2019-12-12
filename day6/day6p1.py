95d8fec8de998e06f31789b072b7933403124ce8
Michal <michalprelich@gmail.com>
2019-12-12 12:02:58 +0100

    day6 p1 works

--- day6/day6p1.py
+++ day6/day6p1.py
@@ -1,6 +1,22 @@
 f = open("file.txt", "r")
-file = f.readline()
+file = f.readlines()
 f.close()
 
 allPlanets = set()
 orbits = {}
+
+for line in file:
+    pt = line.split(')')
+    pt1 = pt[0]
+    pt2 = pt[1][:3] # '\n' at end: XXX\n
+    allPlanets.add(pt1)
+    allPlanets.add(pt2)
+    orbits[pt2] = pt1  
+
+orbitsSum = 0
+for planet in allPlanets:
+    while planet in orbits.keys():
+        planet = orbits[planet]
+        orbitsSum += 1
+
+print(orbitsSum)
